ec696f28d3ee70d7df36627970c4db2ba5833b06
Parker Watkins <Parker.Watkins@imail.org>
2020-11-25 13:56:12 -0700

    added menu and matMult

--- main.py
+++ main.py
@@ -21,16 +21,26 @@ def getEig(matrix):
     print("Eigen Values: {} '\n' Eigen Vectors:\n {}".format(w.real, v))
     return w.real, v
 
+def getX0(R):
+    print("Enter the entries of x0 in a single line ({} values separated by spaces) : ".format(R))
+    # User input of entries in a
+    # single line separated by space
+    entries = list(map(float, input().split()))
+    vector = np.array(entries).reshape(R, 1)
+    print("You entered:\n {}".format(vector))
+    return vector
+
+def multPred(matrix, R):
+    years = int(input("Number of years to predict: "))
+    x = getX0(R)
+    for i in range(years):
+        x = np.matmul(matrix, x)
+    print("The population at {} years is {}.".format(years, x))
 
 def predict(matrix, w, v, R):
     p = int(input("To predict enter number of years or 0 to exit: "))
     if p != 0:
-        print("Enter the entries of x0 in a single line ({} values separated by spaces) : ".format(R))
-        # User input of entries in a
-        # single line separated by space
-        entries = list(map(float, input().split()))
-        vector = np.array(entries).reshape(R, 1)
-        print("You entered:\n {}".format(vector))
+        vector = getX0(R)
         aMatrix = np.concatenate((matrix, vector), 1)
         print("Your augmented matrix is:\n {} ".format(aMatrix))
         vector = np.flipud(vector)
@@ -61,7 +71,33 @@ def predict(matrix, w, v, R):
                 s += part[j]
         print("In {0} years there will be the sum of {1}, or {2} total".format(p, pred, s))
 
+def menu():
+    print("Please make a selection from the menu below", "\n", 
+    "[e] Get eigen values and vectors", "\n",  
+    "[p] Predict using linear comb of eigen vals/vects", "\n", 
+    "[m] Predict using matrix multiplication", "\n", 
+    "[q] Quit\n")
+    validChoices = ['e', 'p', 'm', 'q']
+    while True:
+        choice = input("Enter your choice: ")
+        choice = choice.lower()
+        if choice in validChoices:
+            return choice
+        else:
+            print("Choose a valid option.")
+
+
 if __name__ == '__main__':
     matrix, r = getMatrix()
-    w, v = getEig(matrix)
-    predict(matrix, w, v, r)
+    while True:
+        choice = menu()
+        if choice == 'e':
+            w, v = getEig(matrix)
+        elif choice == 'p':
+            w, v = getEig(matrix)
+            predict(matrix, w, v, r)
+        elif choice == 'm':
+            multPred(matrix, r)
+        else:
+            print("Good bye")
+            break
